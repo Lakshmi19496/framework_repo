2a02763ba3208103385efad665bad5f3b7082226
Lakshmi19496 <satrasala.nagalakshmi@gmail.com>
2024-08-16 15:38:07 +0530

    push framework

---
+++ src/test/java/createorgTest/CreateContactTest.java
@@ -0,0 +1,15 @@
+package createorgTest;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	@Test
+	public void createContactTest() {
+	System.out.println("====createContactTest====");	
+	}
+	@Test
+	public void modifyContactTest() {
+	System.out.println("====createContactTest====");	
+	}
+
+}
---
+++ src/test/java/createorgTest/CreateOrgTest.java
@@ -0,0 +1,18 @@
+package createorgTest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest
+{
+	@Test
+	public void createOrgTest() {
+		String URL=System.getProperty("url");
+		System.out.println(URL);
+	System.out.println("====creATEORG====");	
+	}
+	@Test
+	public void modifyOrgTest() {
+	System.out.println("====modifyOrg====");	
+	}
+
+}
